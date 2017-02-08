e5fbf7de961359af03ce3be6e862462c5be66d0b
egott <emilygottlieb8@gmail.com>
2017-02-08 17:07:47 -0500

    change associations to through instead of belongs to and has many

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,5 +1,7 @@
 class Pokemon < ApplicationRecord
   validates_presence_of :name
   has_many :types
-  has_and_belongs_to_many :games
+  has_many :pokemon_games
+  has_many :games, :through => :pokemon_games
+  # has_and_belongs_to_many :games
 end
