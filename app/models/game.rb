811d42914f27fc783fb0547502cc624f0464d1fb
egott <emilygottlieb8@gmail.com>
2017-02-08 18:25:41 -0500

    change devise to have users

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,7 +1,10 @@
 class Game < ApplicationRecord
-  # has_and_belongs_to_many :pokemons
   belongs_to :user
   validates :name, presence: true
   has_many :pokemon_games
   has_many :pokemons, :through => :pokemon_games
+
+  def winner
+  end
+  
 end
--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -3,5 +3,4 @@ class Pokemon < ApplicationRecord
   has_many :types
   has_many :pokemon_games
   has_many :games, :through => :pokemon_games
-  # has_and_belongs_to_many :games
 end
