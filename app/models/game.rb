c8acb144cf0ba0656fe4d34754b0fd69c1081ff9
[01;31m[K"name"[m[K: "Felix Vandelanotte <[01;31m[K"email"[m[K: "vandelanottefelix@gmail.com>
2017-02-09 02:15:09 +0100

    implement types where user has half damage from

--- app/models/game.rb
+++ app/models/game.rb
@@ -4,7 +4,19 @@ class Game < ApplicationRecord
   has_many :pokemon_games
   has_many :pokemons, :through => :pokemon_games
 
+  def half_damage_from
+    half_from_types = []
+    self.pokemons.each do |pokemon|
+      pokemon.types.each do |type|
+        half_from_types.concat(type.half_damage_from)
+      end
+    end
+    half_from_types.uniq
+  end
+
+
   def winner
   end
-  
+
+
 end
